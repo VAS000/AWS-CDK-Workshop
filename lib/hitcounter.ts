f3c40eec597eba43e94922ef389258dd01f6a782
Vasken Khadar <>
2020-03-08 19:22:53 +1100

    Updated HitCounter contruct + added that to the stack

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -1,12 +1,33 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as dynamoDB from '@aws-cdk/aws-dynamodb';
 
 export interface HitCounterProps {
     downstream: lambda.IFunction,
 };
 
 export class HitCounter extends cdk.Construct {
-    constructor(scope: cdk.Construct, id: string, props?: HitCounterProps) {
+
+    public readonly handler: lambda.IFunction;
+
+    constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
         super(scope, id);
+
+        const table = new dynamoDB.Table(this, 'HitsCounter', {
+            partitionKey: {
+                name: 'path',
+                type: dynamoDB.AttributeType.STRING,
+            },
+        });
+
+        this.handler = new lambda.Function(this, 'HitsCounterHandler', {
+            runtime: lambda.Runtime.NODEJS_12_X,
+            handler: 'hitcounter.handler',
+            code: lambda.Code.fromAsset('lambda'),
+            environment: {
+                DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+                HITS_TABLE_NAME: table.tableName,
+            }
+        });
     }
 }
